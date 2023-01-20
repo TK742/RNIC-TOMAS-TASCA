a20bde7ced2b2c92153ae89d574348d112f24a32
TK742 <ttasca742@gmail.com>
2023-01-20 12:54:51 -0300

    RNIC-W02: new version downloaded

--- ios/myappTests/myappTests.m
+++ ios/myappTests/myappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rnicttTests : XCTestCase
+@interface myappTests : XCTestCase
 
 @end
 
-@implementation rnicttTests
+@implementation myappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
